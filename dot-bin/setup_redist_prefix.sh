e7915eabe21497ba391f250aa1f1dc49c62027bb
Gan-Fall <ale.hdez.cruz@gmail.com>
2025-01-04 00:20:06 -0500

    started wine script to set up prefixes with redists installed

---
+++ dot-bin/setup_redist_prefix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+GAME_DIR=""
+#WINEARCH="win32" WINEPREFIX="${GAME_DIR}/.wine" winecfg
+#WINEARCH="win32" WINEPREFIX="${GAME_DIR}/.wine" setup_dxvk install
+#WINEARCH="win32" WINEPREFIX="${GAME_DIR}/.wine" setup_vkd3d_proton install
+#WINEARCH="win32" WINEPREFIX="${GAME_DIR}/.wine" winetricks atmlib corefonts gdiplus msxml3 msxml6 vcrun2008 vcrun2010 vcrun2012 fontsmooth-rgb
+
+#for COD BO2
+#WINEARCH="win32" WINEPREFIX="${GAME_DIR}/.wine" winetricks atmlib corefonts gdiplus msxml3 msxml6 vcrun2008 vcrun2010 vcrun2012 fontsmooth-rgb dotnet48 xact
