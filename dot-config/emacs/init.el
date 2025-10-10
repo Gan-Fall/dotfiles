f976eb45fda06a71ec8ad700497950f42cb60ee9
Gan-Fall <ale.hdez.cruz@gmail.com>
2025-10-10 18:37:30 -0400

    Cleaned up a bit more.

--- dot-config/emacs/init.el
+++ dot-config/emacs/init.el
@@ -292,15 +292,15 @@
 
 (use-package evil
   :init
-  					; (setq evil-want-integration t) ; Not sure if deprecated.
+  ; (setq evil-want-integration t) ; Not sure if deprecated.
   (setq evil-want-keybinding nil) ; Remove some evil keybinds in other modes which according to system crafters "aren't consistent". TODO: Look into this.
-  					; (setq evil-want-C-i-jump nil) ; Remove vim C-i in edit mode functionality
+  ; (setq evil-want-C-i-jump nil) ; Remove vim C-i in edit mode functionality
   (setq evil-want-C-u-scroll t)
-  					; (setq evil-want-C-u-delete t) ; Whether C-u should delete indent in insert mode.
+  ; (setq evil-want-C-u-delete t) ; Whether C-u should delete indent in insert mode.
   :config
   (evil-set-undo-system 'undo-tree)
   (evil-mode)
-  					; (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join) ; Preserve emacs C-h to backspace
+  ;(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join) ; Preserve emacs C-h to backspace
 
   ;; Make screen recenter after jump
   (defun my/evil-scroll-down ()
@@ -347,21 +347,21 @@
   ;; Set return in normal state to do default action on object
   (evil-define-key 'normal 'global (kbd "RET") 'embark-dwim)
 
-  					;Alternate method
-  					;(define-key evil-normal-state-map (kbd "C-d") #'my/evil-scroll-down)
-  					;(define-key evil-normal-state-map (kbd "C-u") #'my/evil-scroll-up)
+  ;Alternate method
+  ;(define-key evil-normal-state-map (kbd "C-d") #'my/evil-scroll-down)
+  ;(define-key evil-normal-state-map (kbd "C-u") #'my/evil-scroll-up)
 
   ;; Disabled for now as I like jumping with relative numbers between folds.
-  					; J and K will go to the next "wrapped" line (i.e. the same line but wrapped because it is too long)
+  ; J and K will go to the next "wrapped" line (i.e. the same line but wrapped because it is too long)
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-  					; Make Control-g work like Control-c
+  ; Make Control-g work like Control-c
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state))
 
 ;; Not sure what this does in system crafters' config
-  					;(evil-set-initial-state 'messages-buffer-mode 'normal)
-  					;(evil-set-initial-state 'dashboard-mode 'normal)
+  ;(evil-set-initial-state 'messages-buffer-mode 'normal)
+  ;(evil-set-initial-state 'dashboard-mode 'normal)
 
 ;; Remember on certain buffers you might want to start on emacs mode instead of evil mode. If you find any add them here.
 
